7dc97a744b7867cefe62ac5a1c13d80f1839b7d7
Paul Hickman <PaulHickman123@googlemail.com>
2019-05-31 17:09:31 +0100

    > Updated premake to use static runtime

--- Osiris/vendor/GLAD/premake5.lua
+++ Osiris/vendor/GLAD/premake5.lua
@@ -1,6 +1,7 @@
 project "Glad"
     kind "StaticLib"
     language "C"
+	staticruntime "on"
     
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -18,9 +19,12 @@ project "Glad"
 	}
     
 	filter "system:windows"
-        buildoptions { "-std=c11", "-lgdi32" }
 		systemversion "latest"
-        staticruntime "On"
         
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+	
+	filter "configurations:Release"
+		runtime "Debug"
+		symbols "on"
