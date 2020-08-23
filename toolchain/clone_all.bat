856ad48b94304860e67d369e8c9872b591f215db
LordSquall <paulhickman123@googlemail.com>
2020-08-23 22:46:16 +0100

    - Added Pull/Clone toolchain files

---
+++ toolchain/clone_all.bat
@@ -0,0 +1,12 @@
+git clone https://github.com/glfw/glfw
+git clone https://github.com/kbranigan/Simple-OpenGL-Image-Library
+git clone https://github.com/g-truc/glm
+git clone --branch docking https://github.com/ocornut/imgui
+git clone https://github.com/lua/lua
+git clone https://github.com/vinniefalco/LuaBridge
+git clone https://github.com/gabime/spdlog
+git clone https://github.com/SpartanJ/efsw
+git clone https://github.com/hjiang/jsonxx.git
+git clone https://github.com/mono/mono.git
+
+setx OSIRIS_EXTERN_REPOS %CD%
---
+++ toolchain/pull_all.bat
@@ -0,0 +1,26 @@
+cd glfw
+git pull
+cd ..
+cd Simple-OpenGL-Image-Library
+git pull
+cd ..
+cd glm
+git pull
+cd ..
+cd imgui
+git pull
+cd ..
+cd spdlog
+git pull
+cd ..
+cd efsw
+git pull
+cd ..
+cd jsonxx
+git pull
+cd ..
+cd mono
+git pull
+cd ..
+
+setx OSIRIS_EXTERN_REPOS %CD%
