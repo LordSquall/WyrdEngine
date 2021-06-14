66b43a83c597c3649451ce17839a33ec097b4b0b
Paul Richard Hickman <paulhickman123@googlemail.com>
2021-06-14 23:47:55 +0100

    - added additional comments

--- Apps/Editor/src/export/ExportManager.cpp
+++ Apps/Editor/src/export/ExportManager.cpp
@@ -46,6 +46,8 @@ namespace Wyrd::Editor
 		std::ofstream core;
 		core.open(Utils::GetBuildsFolder() + "/Core.dat", std::ios::out | std::ios::binary);
 
+		// Add core configuration settings
+
 		core.close();
 	}
 
@@ -57,30 +59,33 @@ namespace Wyrd::Editor
 		std::ofstream game;
 		game.open(Utils::GetBuildsFolder() + "/Game.dat", std::ios::out | std::ios::binary);
 
+		// Add game settings
+
 		game.close();
 	}
 
 	void ExportManager::GenerateCommonBundleFile()
 	{
+		/* retrieve services */
 		auto resourceService = ServiceManager::Get<ResourceService>(ServiceManager::Resources);
 		auto workspaceService = ServiceManager::Get<WorkspaceService>(ServiceManager::Workspace);
 
+		/* open the common bundle stream */
 		std::ofstream commonBundle;
 		commonBundle.open(Utils::GetBuildsFolder() + "/common.bundle", std::ios::out | std::ios::binary);
 
+		/* we need to get a map of just the texture resources */
 		std::map<UID, TextureRes*> textures;
-
 		for (auto& res : resourceService->GetResources())
 		{
-			switch (res.second->GetType())
+			if (res.second->GetType() == ResourceType::TEXTURE)
 			{
-			case ResourceType::TEXTURE:
 				textures.insert({ res.first, std::dynamic_pointer_cast<TextureRes>(res.second).get() });
-				break;
 			}
 		}
 
-		/* Disabled shader export until resources are resolved */
+		/* NOTE: Disabled shader export until resources are resolved */
+		/* add the shader resources */
 		size_t shaderCount = Application::Get().GetResources().Shaders.size();
 		shaderCount = 0;
 		commonBundle.write((char*)&shaderCount, sizeof(size_t));
@@ -102,6 +107,7 @@ namespace Wyrd::Editor
 			commonBundle.write((char*)fragSrc.c_str(), sizeof(char) * fragSrcSize);
 		}*/
 
+		/* add the texture resources */
 		size_t textureCount = textures.size();
 		commonBundle.write((char*)&textureCount, sizeof(size_t));
 		for (auto& t : textures)
@@ -119,72 +125,77 @@ namespace Wyrd::Editor
 			commonBundle.write((char*)data, sizeof(unsigned char) * (width * height * 4));
 		}
 
+
+		/* build a list of the script files */
+		std::map<UID, std::string> scriptFiles;
+		for (auto& [key, value] : resourceService->GetResources())
 		{
-			std::map<UID, std::string> scriptFiles;
-			for (auto& [key, value] : resourceService->GetResources())
+			auto downcastedPtr = std::dynamic_pointer_cast<ScriptRes>(value);
+			if (downcastedPtr)
 			{
-				auto downcastedPtr = std::dynamic_pointer_cast<ScriptRes>(value);
-				if (downcastedPtr)
-				{
-					scriptFiles.insert({ key , value->GetPath() });
-				}
+				scriptFiles.insert({ key , value->GetPath() });
 			}
-
-			size_t fileCount = scriptFiles.size();
-			commonBundle.write((char*)&fileCount, sizeof(size_t));
-			for (auto& file : scriptFiles)
-			{
-				char fileNamebuffer[64];
-				strcpy(fileNamebuffer, Utils::GetFilename(file.second).c_str());
-				commonBundle.write((char*)&fileNamebuffer, sizeof(char) * 64);
-
-				char uuidBuffer[64];
-				strcpy(uuidBuffer, file.first.str().c_str());
-				commonBundle.write((char*)&uuidBuffer, sizeof(char) * 64);
-			}
-
-			std::string coreLibraryLocation = "C:/Projects/Wyrd/WyrdEngine/lib/Debug/WyrdAPI/WyrdAPI.dll";
-			std::string clientLibraryLocation = Utils::GetPath(workspaceService->GetLoadedProjectPath()) + "\\" + workspaceService->GetCurrentProject()->name + ".dll";
-
-			std::ifstream coreLibFileStream(coreLibraryLocation.c_str(), std::ios::binary);
-			std::vector<char> coreLibData((std::istreambuf_iterator<char>(coreLibFileStream)), (std::istreambuf_iterator<char>()));
-			coreLibFileStream.close();
-
-			std::ifstream clientLibFileStream(clientLibraryLocation.c_str(), std::ios::binary);
-			std::vector<char> clientLibData((std::istreambuf_iterator<char>(clientLibFileStream)), (std::istreambuf_iterator<char>()));
-			clientLibFileStream.close();
-
-			size_t coreLibFileSize = coreLibData.size(); 
-			size_t clientLibFileSize = clientLibData.size();
-
-			commonBundle.write((char*)&coreLibFileSize, sizeof(size_t));
-			commonBundle.write(&coreLibData[0], sizeof(char) * coreLibFileSize);
-
-			commonBundle.write((char*)&clientLibFileSize, sizeof(size_t));
-			commonBundle.write(&clientLibData[0], sizeof(char) * clientLibFileSize);
 		}
+
+		/* write the script file map */
+		size_t fileCount = scriptFiles.size();
+		commonBundle.write((char*)&fileCount, sizeof(size_t));
+		for (auto& file : scriptFiles)
+		{
+			char fileNamebuffer[64];
+			strcpy(fileNamebuffer, Utils::GetFilename(file.second).c_str());
+			commonBundle.write((char*)&fileNamebuffer, sizeof(char) * 64);
+
+			char uuidBuffer[64];
+			strcpy(uuidBuffer, file.first.str().c_str());
+			commonBundle.write((char*)&uuidBuffer, sizeof(char) * 64);
+		}
+
+		/* TODO: Remove hardcoded path */
+		/* load the client and core library and add to the file stream */
+		std::string coreLibraryLocation = "C:/Projects/Wyrd/WyrdEngine/lib/Debug/WyrdAPI/WyrdAPI.dll";
+		std::string clientLibraryLocation = Utils::GetPath(workspaceService->GetLoadedProjectPath()) + "\\" + workspaceService->GetCurrentProject()->name + ".dll";
+
+		std::ifstream coreLibFileStream(coreLibraryLocation.c_str(), std::ios::binary);
+		std::vector<char> coreLibData((std::istreambuf_iterator<char>(coreLibFileStream)), (std::istreambuf_iterator<char>()));
+		coreLibFileStream.close();
+
+		std::ifstream clientLibFileStream(clientLibraryLocation.c_str(), std::ios::binary);
+		std::vector<char> clientLibData((std::istreambuf_iterator<char>(clientLibFileStream)), (std::istreambuf_iterator<char>()));
+		clientLibFileStream.close();
+
+		size_t coreLibFileSize = coreLibData.size(); 
+		size_t clientLibFileSize = clientLibData.size();
+
+		commonBundle.write((char*)&coreLibFileSize, sizeof(size_t));
+		commonBundle.write(&coreLibData[0], sizeof(char) * coreLibFileSize);
+
+		commonBundle.write((char*)&clientLibFileSize, sizeof(size_t));
+		commonBundle.write(&clientLibData[0], sizeof(char) * clientLibFileSize);
 
 		commonBundle.close();
 	}
 
 	void ExportManager::GenerateSceneBundleFile(const Wyrd::UID& sceneUID, const std::string& dir)
 	{
-		std::ofstream sceneBundle;
-		sceneBundle.open(dir + sceneUID.str() + ".bundle", std::ios::out | std::ios::binary);
-
-		/* Load the matching scene */
 		auto resourceService = ServiceManager::Get<ResourceService>(ServiceManager::Resources);
 		auto sceneRes = resourceService->GetResourceByID<SceneRes>(sceneUID);
 
+		/* open the scene file stream */
+		std::ofstream sceneBundle;
+		sceneBundle.open(dir + sceneUID.str() + ".bundle", std::ios::out | std::ios::binary);
+
+		/* load the scene */
 		Scene scene;
 		scene.Initialise();
 		SceneLoader::Load(sceneRes->GetPath(), scene);
 
+		/* write the camera entity */
 		Entity cameraEntity = scene.GetPrimaryCameraEntity();
 		sceneBundle.write((char*)&cameraEntity, sizeof(Entity));
 
+		/* write the entity list */
 		size_t entityCount = scene.entities.size();
-
 		sceneBundle.write((char*)&entityCount, sizeof(size_t));
 		for (auto& e : scene.entities)
 		{
@@ -194,8 +205,8 @@ namespace Wyrd::Editor
 			sceneBundle.write((char*)&mask, sizeof(uint64_t));
 		}
 
+		/* write the component pool arrays */
 		size_t componentPoolCount = scene.componentPools.size();
-
 		sceneBundle.write((char*)&componentPoolCount, sizeof(size_t));
 		for (auto& cp : scene.componentPools)
 		{
