7e6cf7d50ce62224c37f33ac51624e8ad718211a
Lord_Squall <PaulHickman123@googlemail.com>
2023-06-09 23:46:00 +0100

    Update setup-environment.bat

--- buildsystem/windows/setup-environment.bat
+++ buildsystem/windows/setup-environment.bat
@@ -4,3 +4,35 @@ mkdir cmakedir
 cd cmakedir
 cmake ../
 cd ../../..
+
+
+
+git clone https://github.com/g-truc/glm dependencies/glm
+cd dependencies/glm
+mkdir cmakedir
+cd cmakedir
+cmake ../
+cd ../../..
+
+git clone https://github.com/hjiang
+
+git clone https://github.com/gabime/spdlog dependencies/spdlog
+cd dependencies/spdlog
+mkdir cmakedir
+cd cmakedir
+cmake ../
+cd ../../.. 
+
+git clone https://github.com/graeme-hill/crossguid.git dependencies/crossguid
+cd dependencies/crossguid
+mkdir cmakedir
+cd cmakedir
+cmake ../
+cd ../../..
+
+git clone https://github.com/kbranigan/Simple-OpenGL-Image-Library dependencies/soil
+cd dependencies/soil
+mkdir cmakedir
+cd cmakedir
+cmake ../
+cd ../../.. 
