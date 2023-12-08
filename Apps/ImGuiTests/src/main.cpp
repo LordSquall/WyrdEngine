d6c714ca79c877ba282bd194228a59ecc586505a
Paul Richard Hickman <paulhickman123@googlemail.com>
2023-12-08 20:25:33 +0000

    - Added config saving for position and size to the ImGui Test App

--- Apps/ImGuiTests/src/main.cpp
+++ Apps/ImGuiTests/src/main.cpp
@@ -19,21 +19,41 @@ public:
 
 	~ImGuiTestApp()
 	{
-		//system("pause");
+		std::ofstream configFile("config.csv");
+
+		configFile << GetWindow().GetX() << "\n";
+		configFile << GetWindow().GetY() << "\n";
+		configFile << GetWindow().GetWidth() << "\n";
+		configFile << GetWindow().GetHeight() << "\n";
+		configFile.close();
 	}
 };
 
 Wyrd::Application* Wyrd::CreateApplication(int argc, char** argv)
 {
-	LayerProps properties = LayerProps();
+	LayerProps props = LayerProps();
+	props.windowProps.Title = "ImGui Test App";
 
-	/* Process command line args */
-	for (int count{ 0 }; count < argc; ++count)
+	std::ifstream configFile("config.csv");
+	
+	if (configFile.good())
 	{
-		
+		std::string line;
+
+		std::getline(configFile, line);
+		props.windowProps.X = stoi(line);
+
+		std::getline(configFile, line);
+		props.windowProps.Y = stoi(line);
+
+		std::getline(configFile, line);
+		props.windowProps.Width = stoi(line);
+
+		std::getline(configFile, line);
+		props.windowProps.Height = stoi(line);
 	}
 
-	return new ImGuiTestApp(properties);
+	return new ImGuiTestApp(props);
 }
 
 void Wyrd::OnPreAppCreation(Wyrd::Application* application) { }
